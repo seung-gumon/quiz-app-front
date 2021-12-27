14a402fcf3c6431a22392715253f5e8739fd7924
SeungGumon <b9327912@gmail.com>
2021-12-28 00:03:03 +0900

    MaxWidth 설정

--- src/routes/quizStartPresenter.tsx
+++ src/routes/quizStartPresenter.tsx
@@ -15,13 +15,13 @@ const QuizStartPresenter: React.FC<IQuizStartPresenter> =
          gameStart
      }) => {
         return (
-            <main className={'w-full flex justify-center items-center h-screen box-border overflow-hidden fixed'}>
-                <section className={'p-10 w-10/12 md:w-3/6 lg:w-2/6 bg-white rounded-lg'}>
-                    <label htmlFor="userName" className="block mb-2 text-xs md:text-lg font-bold">게임에 참여하는 분의 성함을 적어주세요 !️</label>
+            <main className={'w-full pb-14 lg:pb-0 flex justify-center items-center h-screen box-border overflow-hidden fixed'}>
+                <section className={'p-10 w-10/12 md:w-3/6 lg:w-2/6 bg-white rounded-lg'}  style={{maxWidth:'500px'}}>
+                    <label htmlFor="userName" className="block mb-2 text-sm md:text-lg font-bold">게임에 참여하는 분의 성함을 적어주세요 !️</label>
                     <input type="text" id="userName"
                            value={name || ''}
                            onChange={(event: React.FormEvent<HTMLInputElement>) => setName(event.currentTarget.value)}
-                           className="border border-gray-300 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 "
+                           className="border border-gray-300 text-base rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 "
                            placeholder="ex) 홍길동" required={true} minLength={1}/>
                     <button
                         onClick={gameStart}

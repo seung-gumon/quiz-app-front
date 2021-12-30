667e8552e06ed9c35337b7d850111a37a0c8e226
SeungGumon <b9327912@gmail.com>
2021-12-31 00:04:36 +0900

    download react-apexcharts

--- src/routes/quiz/quizResult/QuizResultPresenter.tsx
+++ src/routes/quiz/quizResult/QuizResultPresenter.tsx
@@ -13,7 +13,17 @@ const QuizResultPresenter : React.FC<IQuizResultPresenter> =
      }) => {
 
     return (
-        <div>삐빅 이곳은 결과 창입니다!</div>
+        <main>
+            <section
+                className={'animate-fade-in-up w-full lg:pb-0 flex justify-center items-center h-screen box-border overflow-hidden fixed'}>
+                <article className={'p-6 w-10/12 md:w-5/6 lg:w-4/6 bg-white rounded-lg'}>
+                    <div>
+
+                    </div>
+                    <span>Hello spider man</span>
+                </article>
+            </section>
+        </main>
     )
 }
 

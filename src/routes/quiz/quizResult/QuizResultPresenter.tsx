f124b560335a27696747ec703754f3d7c9a5c823
SeungGumon <b9327912@gmail.com>
2021-12-31 01:10:13 +0900

    그래프생성중

--- src/routes/quiz/quizResult/QuizResultPresenter.tsx
+++ src/routes/quiz/quizResult/QuizResultPresenter.tsx
@@ -1,30 +1,64 @@
 import React from 'react';
-
+import ApexChart from "react-apexcharts";
 
 interface IQuizResultPresenter {
 
 }
 
 
+const QuizResultPresenter: React.FC<IQuizResultPresenter> =
+    ({}) => {
 
-const QuizResultPresenter : React.FC<IQuizResultPresenter> =
-    ({
 
-     }) => {
+        return (
+            <main>
+                <section
+                    className={'animate-fade-in-up w-full lg:pb-0 flex justify-center items-center h-screen box-border overflow-hidden fixed'}>
+                    <article className={'p-6 w-10/12 md:w-5/6 lg:w-4/6 bg-white rounded-lg'}>
+                        <div className={'flex items-center justify-center flex-col'}>
+                            <ApexChart
+                                width={400}
+                                type={'pie'}
+                                series={
+                                    [4, 6]
+                                }
+                                options={{
+                                    labels: ['정답', '오답'],
+                                    legend: {
+                                        position: "bottom",
+                                        fontSize : "16px"
+                                    },
+                                    responsive: [
+                                        {
+                                            breakpoint: 600,
+                                            options: {
+                                                chart: {
+                                                    width: "100%",
+                                                    type: 'pie',
+                                                },
+                                            }
+                                        }
+                                    ]
+                                }}
+                            />
+                            <div className={'flex flex-col w-full items-center justify-center '}>
+                                <span>총 10문제</span>
+                                <div className={'flex md:flex-col'}>
+                                    <span>정답 : 1</span>
+                                    <span>오답 : 2</span>
+                                </div>
 
-    return (
-        <main>
-            <section
-                className={'animate-fade-in-up w-full lg:pb-0 flex justify-center items-center h-screen box-border overflow-hidden fixed'}>
-                <article className={'p-6 w-10/12 md:w-5/6 lg:w-4/6 bg-white rounded-lg'}>
-                    <div>
+                                <div className={'flex w-full flex-col md:flex-row items-center justify-center'}>
+                                    <button className={'md:px-3'}>오답 풀기</button>
+                                    <button className={'md:px-1.5'}>새로운 문제 풀기</button>
+                                </div>
+                            </div>
 
-                    </div>
-                    <span>Hello spider man</span>
-                </article>
-            </section>
-        </main>
-    )
-}
+                        </div>
+                    </article>
+                </section>
+            </main>
+        )
+    }
 
 export default QuizResultPresenter
